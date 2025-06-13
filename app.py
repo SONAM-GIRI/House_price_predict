195072bd172a1329af68811632ff3752a51d4391
SONAM-GIRI <sonamgiri773@gmail.com>
2025-06-13 11:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
